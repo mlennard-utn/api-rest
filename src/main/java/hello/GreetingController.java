2a9048a60d5b84d0c1030edb4fe2a4a3f0a38786
alex de assis <alexdeassis@gmail.com>
2018-11-22 22:22:13 -0300

    API rest

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,40 @@
+package hello;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+    
+    @RequestMapping("/nacionalidades")
+    public List<Nacionalidad> nacionalidades() {
+        return getListNacionalidades();
+    }
+    
+    private List<Nacionalidad> getListNacionalidades(){
+    	List<Nacionalidad> lista = new ArrayList<>();
+    	
+    	for (int i = 1; i <= 3; i++) {
+			lista.add(new Nacionalidad(i, "nacionalidad-"+i));
+		}
+    	
+    	return lista;
+    }
+    
+}
---
+++ src/main/java/hello/Nacionalidad.java
@@ -0,0 +1,26 @@
+package hello;
+
+public class Nacionalidad {
+	
+	private Integer id;
+	private String nombre;
+	
+	public Nacionalidad(Integer id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+}
